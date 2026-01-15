f154e1fc3ab9103346146d51f224929e30bd4815
Chang Chih Hsuan <24037723@myrp.edu.sg>
2026-01-15 11:44:37 +0800

    notes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const dbConfig= {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    // ssl: { rejectUnauthorized: false },
+    ssl: { rejectUnauthorized: false },
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit: 0,
